f73a31f624b38584361ac845981bdf325b8e87f3
LeaBalasko <153901227+LeaBalasko@users.noreply.github.com>
2024-02-02 09:29:16 +0100

    Add files via upload

---
+++ aplikacija-2/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/onama")
+def onama():
+    return render_template("o_nama.html")
+
+@app.route("/usluge")
+def usluge():
+    return render_template("usluge.html")
+
+if __name__ == "__main__":
+    app.run()
